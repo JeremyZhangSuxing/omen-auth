e7152b1e9f9cea35a09ede179b23198041224805
knight <suxing.zhang3@wework.com>
2021-09-22 20:45:34 +0800

    docker install mysql version > 8.0 lock the version of connector

--- omen-security-demo/src/main/java/com/imooc/DemoApplication.java
+++ omen-security-demo/src/main/java/com/imooc/DemoApplication.java
@@ -2,6 +2,7 @@ package com.imooc;
 
 import com.imooc.browser.BrowserSecurityConfig;
 import com.imooc.browser.web.controller.BrowserController;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -12,12 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @author : Knight
  * @date : 2021/8/29 10:37 上午
  */
+@Slf4j
 @RestController
 @SpringBootApplication(scanBasePackageClasses = {BrowserSecurityConfig.class, DemoApplication.class})
 public class DemoApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(DemoApplication.class, args);
-        System.out.print(run.getBean(BrowserController.class).toString());
+        log.info("DemoApplication  < ------ >  {}", run.getBean(BrowserController.class));
     }
 
     @GetMapping("/hello")
